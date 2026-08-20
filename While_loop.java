86a88fab1e3f7d3b5622c6a42b89e4371aad6830
Sujalkhandagle <sujalkhandagle928@gmail.com>
2026-08-20 11:36:16 +0530

    hvhh

---
+++ While_loop.java
@@ -0,0 +1,22 @@
+public class While_loop {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=10) {
+            System.out.println(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        i=10;
+        while (i>=1) {
+            System.out.println(i+"  ");
+            i--;
+        }
+        int s=1,no=12;
+        while (s<=10) {
+            System.out.println(s+" * "+s+" :="+s*no);
+            s++;
+            
+        }
+    }
+}

d31f39cfeebdb86b4863eb2458d8976c40bd172b
Sujalkhandagle <sujalkhandagle928@gmail.com>
2026-08-20 12:11:47 +0530

    bjg

--- Loop.java
+++ Loop.java
@@ -1,5 +1,17 @@
 public class Loop {
     public static void main(String[] args) {
-        
+        int no=4554,rem,rev=0,t;
+        t=no;
+        while (no>0) {
+            rem=no%10;
+            rev=rev*10+rem;
+            no=no/10;
+
+        }
+System.out.println("revsre := "+rev);
+if(t==rev)
+    System.out.println(t+" is palidrom");
+else
+    System.out.println(t+" not palidrom");
     }
 }

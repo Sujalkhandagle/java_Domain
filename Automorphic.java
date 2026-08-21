4efce28ee7068e26a07f544eac9a8ab502163ad1
Sujalkhandagle <sujalkhandagle928@gmail.com>
2026-08-21 12:39:59 +0530

    loop

--- Automorphic.java
+++ Automorphic.java
@@ -1,20 +1,31 @@
 public class Automorphic {
     public static void main(String[] args) {
-        int no=25,sq,t;
-		boolean isAutomorphic=true;
-		sq=no*no;
-		t=no;
-		while(no>0) {
-			if(no%10!=sq%10) {
-				isAutomorphic=false;
-				break;
-			}
-			no=no/10;
-			sq=sq/10;
-		}
-		if(isAutomorphic)
-			System.out.println(t+" is Automorphic");
-		else
-			System.out.println(t+" Not Automorphic");
+        // int no=25,sq,t;
+		// boolean isAutomorphic=true;
+		// sq=no*no;
+		// t=no;
+		// while(no>0) {
+		// 	if(no%10!=sq%10) {
+		// 		isAutomorphic=false;
+		// 		break;
+		// 	}
+		// 	no=no/10;
+		// 	sq=sq/10;
+		// }
+		// if(isAutomorphic)
+		// 	System.out.println(t+" is Automorphic");
+		// else
+		// 	System.out.println(t+" Not Automorphic");
+	 int i=1;
+	 int cnt=1;
+	 int n=10;
+	 while (cnt<=n) {
+		System.out.println(i);
+		i=i*10+1  ;
+		cnt+=1;
+		
+	 }
 	}
     }
+
+
---
+++ Nested_loop.java
@@ -0,0 +1,143 @@
+public class Nested_loop {
+ public static void main(String[] args) {
+    // int i=1,j;
+    // while (i<=10) {
+    //     j=1;
+    //     while (j<=10) {
+    //         System.out.print(i*j+" ");
+    //         j++;
+    //     }
+    //     System.out.println();
+    //     i++;
+    // }
+
+// star pattern 
+
+    // int i=1,j;  
+    // while (i<=5) {  
+    //     j=1;
+    //     while (j<=5) {
+    //         System.out.print("* ");
+    //         j++;
+    //     }
+    //     i++;
+    //     System.out.println();
+
+    // }
+
+     
+    //  int i=1,j;  
+    // while (i<=5) {
+    //     j=1;
+    //     while (j<=i) {
+    //         System.out.print("* ");
+    //         j++;
+    //     }
+    //     i++;
+    //     System.out.println();
+
+    // }
+
+    //  int i=1,j;  
+    // while (i<=5) {
+    //     j=i;
+    //     while (j<=5) {
+    //         System.out.print("* ");
+    //         j++;
+    //     }
+    //     i++;
+    //     System.out.println();
+  
+    // }
+
+    //    int i=1,j;  
+    // while (i<=5) {
+    //     j=i;
+    //     while (j<5) {
+    //         System.out.print("    ");
+    //         j++;
+    //     }
+    //     j=1;
+    //     while (j<=i) {
+    //         System.out.print("* ");
+    //         j++;
+    //     }
+    //     i++;
+    //     System.out.println();
+  
+    // }
+
+
+    
+    //    int i=1,j;  
+    // while (i<=5) {
+    //     j=1;
+    //     while (j<i) {
+    //         System.out.print("  ");
+    //         j++;
+    //     }
+    //     j=i;
+    //     while (j<=5) {
+    //         System.out.print("* ");
+    //         j++;
+    //     }
+    //     i++;
+    //     System.out.println();
+  
+    // }
+
+    // frame patern in star impp
+
+//     int i=1,j;  
+//     while (i<=10) {  
+//         j=1;
+//         while (j<=10) {
+
+//             if(i==1 || i==10 || j==1 || j==10)
+//             System.out.print("* ");
+//         else
+//             System.out.print("  ");
+//             j++;
+//         }
+//         i++;
+//         System.out.println();
+   
+//  } 
+
+//   int i=1,j;  
+//     while (i<=10) {  
+//         j=1;
+//         while (j<=10) {
+
+//             if(i==1 || i==10 || j==1 || j==10 || i==j)
+//             System.out.print("* ");
+//         else
+//             System.out.print("  ");
+//             j++;
+//         }
+//         i++;
+//         System.out.println();
+   
+
+// }
+
+
+  int i=1,j;  
+    while (i<=10) {  
+        j=1;
+        while (j<=10) {
+
+            if(i==1 || i==10 || j==1 || j==10 || i==j || i+j==11)
+            System.out.print("* ");
+        else
+            System.out.print("  ");
+            j++;
+        }
+        i++;
+        System.out.println();
+   
+
+}
+}
+}
+    
